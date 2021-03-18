61c6d8822f4b11996109fcddef9f3cefd141aa5f
Dominika <dominika.jansova1011@gmail.com>
2021-03-18 19:43:51 +0100

    welcome-page class changed to start

--- welcome.js
+++ welcome.js
@@ -1,14 +1,14 @@
-const $welcomeText = document.getElementById('welcome-text');
+const $start = document.getElementById('start');
 const $musicContainer = document.getElementById("music-container");
 const $text = document.getElementById('text');
 const $dotContainer = document.getElementById('dot-container');
 
 
-function welcomeText() {
-    $welcomeText.style.display = 'none';
+function welcomePage() {
+    $start.style.display = 'none';
     $musicContainer.style.visibility = 'visible';
     $text.style.display = 'block';
     $dotContainer.style.visibility = 'visible';
 }
 
-$welcomeText.onclick = welcomeText;
+$start.onclick = welcomePage;
