7030e7335d87c33bc73fc494318224e8f24704bc
Dominika <dominika.jansova1011@gmail.com>
2021-03-19 17:25:37 +0100

    function startMediation

--- welcome.js
+++ welcome.js
@@ -2,23 +2,23 @@ const $start = document.getElementById('start');
 const $text = document.getElementById('text');
 const $musicContainer = document.getElementById("music-container");
 const $dotContainer = document.getElementById('dot-container');
-
+const $playBtn = document.getElementById("play");
 
 function showPlayer(){
     $start.style.display = 'none';
     $musicContainer.style.visibility = 'visible';
     $musicContainer.classList.add('animated');
+    $dotContainer.style.visibility = 'visible';
 }
 
 $start.onclick =  showPlayer;
 
 
+function startMeditation() {
+    $start.style.display = 'none';
+    $text.style.display = 'block';
+    $dotContainer.classList.add('circling');
+    $dotContainer.classList.add('circling');
+}
 
-// function startMeditation() {
-//     $start.style.display = 'none';
-   
-//     $text.style.display = 'block';
-//     $dotContainer.style.visibility = 'visible';
-// }
-
-
+$playBtn.onclick =  startMeditation;
