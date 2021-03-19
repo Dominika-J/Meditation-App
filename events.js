ffce2299c11838601ac1e622036a6acf9f71fa3f
Dominika <dominika.jansova1011@gmail.com>
2021-03-19 19:30:50 +0100

    dot paused

--- events.js
+++ events.js
@@ -3,6 +3,9 @@ const $text = document.getElementById('text');
 const $musicContainer = document.getElementById("music-container");
 const $dotContainer = document.getElementById('dot-container');
 const $playBtn = document.getElementById("play");
+const $containerGrow = document.getElementsByClassName("container grow");
+const $containerShrink = document.getElementsByClassName("container grow");
+
 
 function showPlayer(){
     $start.style.display = 'none';
@@ -14,10 +17,22 @@ function showPlayer(){
 
 $start.onclick =  showPlayer;
 
-function startMeditation() {
+function startPauseMeditation() {
     $start.style.display = 'none';
     $text.style.display = 'block';
     $dotContainer.classList.add('circling');
+
+    const runningDot = $dotContainer.style.animationPlayState === 'running';
+    $dotContainer.style.animationPlayState = runningDot ? 'paused' : 'running';
+
+    // const runningGrow = $containerGrow.style.animationPlayState === 'running';
+    // $containerGrow.style.animationPlayState = runningGrow ? 'paused' : 'running';
+
+    // const runningShrink = $containerShrink.style.animationPlayState === 'running';
+    // $containerShrink.style.animationPlayState = runningShrink ? 'paused' : 'running';
+
+
 }
 
-$playBtn.onclick =  startMeditation;
+$playBtn.onclick =  startPauseMeditation;
+
