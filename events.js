b4939a2f9c11aef67d643f3fb334e1aadd58bfa2
Dominika <dominika.jansova1011@gmail.com>
2021-03-20 14:56:32 +0100

    rewrited function showPlayer, handled in CSS

--- events.js
+++ events.js
@@ -1,37 +1,35 @@
-const $start = document.getElementById('start');
-const $text = document.getElementById('text');
-const $musicContainer = document.getElementById("music-container");
-const $dotContainer = document.getElementById('dot-container');
-const $playBtn = document.getElementById("play");
-const $animationControl = Array.from(document.getElementsByClassName("animation-control"));
+const $textStart = document.getElementById('text-start');
+const $body = document.body;
+
+const $animationControl = Array.from(document.getElementsByClassName('animation-control'));
 
 
 function showPlayer(){
-    $start.style.display = 'none';
-    $musicContainer.style.visibility = 'visible';
-    $musicContainer.classList.add('animated');
-    $dotContainer.style.visibility = 'visible';
-    $dotContainer.classList.add('fadeIn');
+    $body.classList.add('initialized');
 }
-
-$start.onclick =  showPlayer;
-
-function startPauseMeditation() {
-    $start.style.display = 'none';
-    $text.style.display = 'block';
-    $dotContainer.classList.add('circling');
-
-    $animationControl.forEach(animation => {
-        const running = animation.style.animationPlayState || 'paused';
-        animation.style.animationPlayState = running === 'running' ? 'paused' : 'running';
-      })
-
-    // const isAnimationRunning = $animationControl.forEach()
-    
-    // style.animationPlayState === 'running';
-    // $animationControl.style.animationPlayState = isAnimationRunning ? 'paused' : 'running';
-}
+    $textStart.onclick =  showPlayer;
 
 
-$playBtn.onclick =  startPauseMeditation;
+// function showPlayer(){
+//     $textStart.style.display = 'none';
+//     $musicContainer.style.visibility = 'visible';
+//     $musicContainer.classList.add('animated');
+//     $dotContainer.style.visibility = 'visible';
+//     $dotContainer.classList.add('fadeIn');
+// }
+
+// $textStart.onclick =  showPlayer;
+
+// function textStartPauseMeditation() {
+//     $textStart.style.display = 'none';
+//     $text.style.display = 'block';
+//     $dotContainer.classList.add('circling');
+
+//     $animationControl.forEach(animation => {
+//         const running = animation.style.animationPlayState || 'paused';
+//         animation.style.animationPlayState = running === 'running' ? 'paused' : 'running';
+//       })
+//     }
+
+// $playBtn.onclick =  textStartPauseMeditation;
 
